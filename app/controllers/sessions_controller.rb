271c4f190647cd12831c4bf5f491a6ee416f8f0e
Anca Sarbu <anca.sarbu@pitechnologies.ro>
2017-07-04 17:15:08 +0300

    Added email to user
    - after sign_in/sign_out redirect to root

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,13 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(request.env['omniauth.auth'])
     session[:user_id] = user.id
-    render json: "Successful authentication of user with id #{user.id}"
+    redirect_to root_path
+    # render json: "Successful authentication of user with id #{user.id}"
   end
 
   def destroy
     session[:user_id] = nil
-    render json: "You were logged out of the application."
+    redirect_to root_path
+    # render json: "You were logged out of the application."
   end
 end
